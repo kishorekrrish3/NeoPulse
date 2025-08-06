9a7dbe869c0e08d54ae36e956a2edcd596264571
Kishore P <kidkrrish3@gmail.com>
2025-08-06 20:37:01 +0530

    Vercel Analytics added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import './globals.css';
 import Script from 'next/script';
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata: Metadata = {
   title: 'NeoPulse - The Future of Smartwatches',
